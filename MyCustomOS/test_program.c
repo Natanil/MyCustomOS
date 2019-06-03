45d52a3aa8b5e70e097c6830118bb9401eff2083
Mohammad Reza Soltaninia <mrsoltaninia@gmail.com>
2019-06-03 15:30:54 +0430

    Test #ifdef macro by writing a test program in test_program.c and learned how to use ifdef

--- MyCustomOS/test_program.c
+++ MyCustomOS/test_program.c
@@ -2,10 +2,17 @@
 #include "stat.h"
 #include "user.h"
 
+#define testDef 1;
+
 int main(void){
-   int* rtime=(int *)1;
-   int* wtime=(int *)100;
-   printf(0,"test ----  %d",getPerformanceData(wtime,rtime));
+ 
+   #ifdef testDef
+      printf(0,"salam");
+   #endif
+
+ //  int* rtime=(int *)1;
+  // int* wtime=(int *)100;
+  // printf(0,"test ----  %d",getPerformanceData(wtime,rtime));
    exit();
 
 }
